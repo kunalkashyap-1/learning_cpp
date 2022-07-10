3b364dd221d1e598b85df39a4199219cca765520
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-10 19:52:04 +0530

    "used amend".

---
+++ practice/STL/array.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include<array>
+using namespace std;
+
+void updateArray(array<int, 6> &ar, int i, int val)
+{
+    ar[i] = val;
+}
+
+void print(array<int, 6> &ar)
+{
+    for (int i = 0; i < ar.size(); i++)
+    {
+        cout << ar[i] << " ";
+    }
+    cout << endl;
+}
+void display(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    // Array classical
+    int arr[] = {5, 4, 3, 2, 1}; // initialization list
+    int n = sizeof(arr) / sizeof(arr[0]);
+    //for each loop
+    for(int a: arr){
+        cout<<a <<" ";
+    }
+    cout<<endl;
+    // array in stl
+    array<int, 6> ar = {1, 3, 2, 5, 4, 6};
+    updateArray(ar, 0, 10);
+    print(ar);
+    sort(ar.begin(), ar.end());
+    print(ar);
+    array<int,10> arrr;
+    arrr.fill(5);
+    for(int a: arrr){
+        cout<<a <<" ";
+    }
+    cout<<endl;
+    return (0);
+}
