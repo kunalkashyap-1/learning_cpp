69266807f9734fd2e46c418d4478e08278e236fd
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-11 21:33:55 +0530

    "-a usde"

--- practice/STL/array.cpp
+++ practice/STL/array.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-#include<array>
+#include <array>//not necessary after the above declaration 
+
 using namespace std;
 
 void updateArray(array<int, 6> &ar, int i, int val)
@@ -15,7 +16,8 @@ void print(array<int, 6> &ar)
     }
     cout << endl;
 }
-void display(int arr[],int n){
+void display(int arr[], int n)
+{
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
@@ -27,22 +29,26 @@ int main()
     // Array classical
     int arr[] = {5, 4, 3, 2, 1}; // initialization list
     int n = sizeof(arr) / sizeof(arr[0]);
-    //for each loop
-    for(int a: arr){
-        cout<<a <<" ";
+    // for each loop
+    for (int a : arr)
+    {
+        cout << a << " ";
     }
-    cout<<endl;
+    cout << endl;
     // array in stl
     array<int, 6> ar = {1, 3, 2, 5, 4, 6};
     updateArray(ar, 0, 10);
     print(ar);
     sort(ar.begin(), ar.end());
     print(ar);
-    array<int,10> arrr;
+    array<int, 10> arrr;
     arrr.fill(5);
-    for(int a: arrr){
-        cout<<a <<" ";
+    for (int a : arrr)
+    {
+        cout << a << " ";
     }
-    cout<<endl;
+    cout << endl;
+    //arr.front()=> array element at the front
+    //arr.back() => array element at the back 
     return (0);
 }
