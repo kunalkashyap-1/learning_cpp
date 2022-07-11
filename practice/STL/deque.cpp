2fb430717b072a69b1bc20600acaf797e101c265
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-11 23:05:52 +0530

    double ended queue

--- practice/STL/deque.cpp
+++ practice/STL/deque.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void display(deque<int> &dq){
+    for(auto x: dq){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
 
 
 int main()
 {
-    
+    deque<int> dq(10);
+    for (int i = 0; i < 10; i++)
+    {
+        dq[i]=i*i;
+    }
+    display(dq);
+    cout<<"size "<<dq.size()<<endl;
+    dq.push_back(20);
+    dq.push_front(13);
+    display(dq);
+    cout<<"size "<<dq.size()<<endl;
     return (0);
 }
