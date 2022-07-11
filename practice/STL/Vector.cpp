7128c02510f5472b2df2513e684cc3027059b6d6
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-11 22:39:47 +0530

    vector

--- practice/STL/Vector.cpp
+++ practice/STL/Vector.cpp
@@ -1,11 +1,42 @@
 #include <bits/stdc++.h>
-#include<vector>
+#include <vector>
 using namespace std;
 
-
-
+void display(vector<int> &vec)
+{
+    for (int x : vec)
+    {
+        cout << x<<" ";
+    }
+    cout << endl;
+}
 int main()
 {
-    
+    // empty container constructor
+    vector<int> vec;
+    // fill constructor (4 intergers with values 20)
+    vector<int> withFull(4, 20);
+    // range constructor
+    int num[] = {33, 45, 65, 77};
+    vector<int> range(num, num + 4);
+    // copy constructor
+    vector<int> copy(range);
+    // vector with initialization list
+    vector<int> init = {34, 54, 67, 54};
+    init.push_back(20);
+    // vec.reserve(100);
+    int n, data;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data" << endl;
+        cin >> data;
+        vec.push_back(data);
+        cout << "capacity " << vec.capacity() << endl
+             << "size " << vec.size() << endl;
+    }
+    display(vec);
+    sort(vec.begin(), vec.end());
+    display(vec);
     return (0);
 }
