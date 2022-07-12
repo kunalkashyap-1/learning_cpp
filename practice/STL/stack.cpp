624599e78c98b94af9b41b69bc884825692b7bf1
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-12 16:51:38 +0530

    Stack

--- practice/STL/stack.cpp
+++ practice/STL/stack.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
+#include<stack>
 using namespace std;
 
-
+void display(stack<int> &st){
+    while(!st.empty()){
+        cout<<st.top();
+    }
+    cout<<endl;
+}
 
 int main()
 {
-    
+    stack<int> st;
+    st.push(23);
+    st.push(43);
+    st.push(44);
+    st.push(2);
+    display(st);
+    st.pop();
+    display(st);
     return (0);
 }
