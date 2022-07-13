a767a3481027ca08b62361bbcc100e660743a034
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-13 16:56:14 +0530

    Phonebook

---
+++ practice/STL/Phonebook.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <string>
+#include <list>
+using namespace std;
+
+void display(map<string, list<string>> &phonebook)
+{
+    // map<string,list<string>>:: iterator iter;
+    for (pair<string, list<string>> iter : phonebook)
+    {
+        cout << iter.first << "\t";
+        for (string x : iter.second)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    map<string, list<string>> phonebook;
+    phonebook["ichigo"].push_back("4652883");
+    phonebook["ichigo"].push_back("46545135");
+
+    phonebook["rukia"].push_back("2288526");
+    phonebook["rukia"].push_back("87956532");
+    display(phonebook);
+    return (0);
+}
--- practice/STL/hashing.cpp
+++ practice/STL/hashing.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
-#include<unordered_map>
-#include<string>
+#include <unordered_map>
+#include <string>
 using namespace std;
-
-
 
 int main()
 {
-    //instert values 
-    unordered_map<string,int> menu;
-    menu["maggi"]=15;
-    menu["momo"]=40;
-    menu["cold_drink"]=45;
-    menu["dosa"]=50;
-    //update query 
-    menu["dosa"]=(1+0.1)*menu["dosa"];
+    // instert values
+    unordered_map<string, int> menu;
+    menu["maggi"] = 15;
+    menu["momo"] = 40;
+    menu["cold_drink"] = 45;
+    menu["dosa"] = 50;
+    // update query
+    menu["dosa"] = (1 + 0.1) * menu["dosa"];
 
-    //deletion in hash table
+    // deletion in hash table
     menu.erase("dosa");
-//searching inside collection of key value pairs 
+    // searching inside collection of key value pairs1
     string item;
-    cin>>item;
-    if(menu.count(item)==0){
-        cout<<"this item is not available"<<endl;
+    cin >> item;
+    if (menu.count(item) == 0)
+    {
+        cout << "this item is not available" << endl;
     }
-    else{
-        cout<<item<<" is available, and cost is "<<menu[item]<<endl;
+    else
+    {
+        cout << item << " is available, and cost is " << menu[item] << endl;
     }
 
-    for(/*auto*/pair<string,int> item: menu){
-        cout<<item.first<<" "<<item.second<<endl;
+    for (/*auto*/ pair<string, int> item : menu)
+    {
+        cout << item.first << " " << item.second << endl;
     }
     return (0);
 }
