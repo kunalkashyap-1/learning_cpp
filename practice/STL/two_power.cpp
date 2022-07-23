989b0dca8e83aa8a2fe30829d28d0e91b25b1038
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-23 19:45:32 +0530

    power of two

--- practice/STL/two_power.cpp
+++ practice/STL/two_power.cpp
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int powerOfTwo(int n){
+    return (n=n&(n-1))?-1:1;
+}
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+    cout<<powerOfTwo(n)<<endl;
     return (0);
 }
