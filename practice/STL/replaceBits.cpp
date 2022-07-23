3465ae846f39ab689535010ac1b4e134005ae7b3
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-23 19:34:25 +0530

    replace bits

--- practice/STL/replaceBits.cpp
+++ practice/STL/replaceBits.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void clearBitsInRange(long int &n, int i, int j)
+{
+    int maska = (-1 << (i+1));
+    int maskb = (1 << j) - 1;
+    int fmask = maska | maskb;
+    n = n & fmask;
+}
 
+int replaceBits(long int &n, long int &m, int i, int j)
+{
+    clearBitsInRange(n, i, j);
+    int mask = (m << j);
+    return (n = n | mask);
+}
 
 int main()
 {
-    
+    long int n = 15;
+    long int m = 2;
+    int i = 3, j = 1;
+    cout << replaceBits(n, m, i, j) << endl;
     return (0);
 }
