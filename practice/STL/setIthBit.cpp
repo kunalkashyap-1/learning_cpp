881534a78ca8e40cd3fe916c4fdb19a0a6dd7adb
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-23 17:22:12 +0530

    23 jul

---
+++ practice/STL/setIthBit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int setIthBit(int &n,int i){
+
+}
+
+int main()
+{
+    int n=5;
+    int i;
+    cin>>i;
+    cout<<setIthBit(n,i);
+    return (0);
+}
