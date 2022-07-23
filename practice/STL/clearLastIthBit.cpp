8e4742db514e0e1ce138f192e83435131330f7f0
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-23 18:11:38 +0530

    clear last ith bit

--- practice/STL/clearLastIthBit.cpp
+++ practice/STL/clearLastIthBit.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int clearLastIthBit(int &n, int i)
+{
+    int mask = (-1<<i);
+    return (n=n&mask);
+}
 
 int main()
 {
-    
+    int n=13;
+    int i;
+    cin>>i; 
+    cout<<clearLastIthBit(n,i)<<endl;
     return (0);
 }
