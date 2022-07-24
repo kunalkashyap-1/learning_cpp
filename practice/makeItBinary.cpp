6ba571dfeff160984ff0d8c35ce142619ac12a96
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-24 14:40:36 +0530

    make it binary

--- practice/makeItBinary.cpp
+++ practice/makeItBinary.cpp
@@ -1,10 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int convertIntoBinary(int n){
+    int ans=0;
+    int pow=1;
+    while(n>0){
+        int last_bit=(n&1);
+        ans+=last_bit*pow;
+        pow*=10;
+        n=n>>1;
+    }
+    return ans;
+}
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+    cout<<convertIntoBinary(n);
     return (0);
 }
---
+++ practice/sortByBits.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortByBits(vector<int> &arr){
+    sort(arr.begin(),arr.end());
+}
+
+int main()
+{
+    
+    return (0);
+}
