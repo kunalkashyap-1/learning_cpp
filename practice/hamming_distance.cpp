b903ba30a00e892e49567643eded2d80d278eb8d
cr1pto <kunal.kashyap.8775@gmail.com>
2022-07-24 16:23:45 +0530

    hamming distance

--- practice/hamming_distance.cpp
+++ practice/hamming_distance.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int hamming_distance(int x,int y){
+    int dis=0;
+    while(x!=0 || y!=0)
+    {
+        if((x&1)!=(y&1)){
+            dis++;
+        }
+        x=x>>1;
+        y=y>>1;
+    }
+    return dis;
+}
 
 int main()
 {
-    
+    int x = 5;
+    int y = 7;
+    cout<<hamming_distance(x,y)<<endl;
     return (0);
 }
